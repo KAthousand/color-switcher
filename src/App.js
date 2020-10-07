bc477e60d0b27b1af04638c1788a9530283d6eb0
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-10-07 18:07:21 -0400

    Kyle Thousand

--- src/App.js
+++ src/App.js
@@ -5,22 +5,40 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
-    }
+      color: "black",
+      textColor: "black",
+    };
   }
-
+  changeColor(newColor) {
+    this.setState({ color: newColor });
+  }
+  changeTextColor(e) {
+    this.setState({ textColor: e.target.value });
+  }
   render() {
     return (
       <div className="app">
-        <h1 style={{ color: "black" }}>Color Switcher</h1>
-        <div id="color-panel" className="black"></div>
+        <h1 style={{ color: this.state.textColor }}>Color Switcher</h1>
+        <div id="color-panel" className={this.state.color}></div>
         <div className="button-container">
-          <button className="red"></button>
-          <button className="yellow"></button>
-          <button className="blue"></button>
-          <button className="green"></button>
+          <button
+            className="red"
+            onClick={() => this.changeColor("red")}
+          ></button>
+          <button
+            className="yellow"
+            onClick={() => this.changeColor("yellow")}
+          ></button>
+          <button
+            className="blue"
+            onClick={() => this.changeColor("blue")}
+          ></button>
+          <button
+            className="green"
+            onClick={() => this.changeColor("green")}
+          ></button>
         </div>
-        <input type="color"/>
+        <input type="color" onChange={(e) => this.changeTextColor(e)} />
       </div>
     );
   }
